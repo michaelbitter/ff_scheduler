a465d7af2f2eac0a716bfc4febed5b6badd94ac0
Michael Bitter <michaelpbitter@gmail.com>
2016-07-07 15:10:46 -0700

    Display a more sophisticated result summary.
    Results from a single draft - even ones that have
    been influenced by many previous drafts - don't
    paint a clear picture of what drafting strategies work
    most often. Now we display an average of each positional
    player draft order from the drafts where it was used.
    
    Add defensive teams into the draft.

--- draft_calculator/draft.py
+++ draft_calculator/draft.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 
 import csv
+import operator
 import pprint
 import random
 
-POPULATION_SIZE = 100000
-GENERATIONS = 10
-FITNESS = 500
+from collections import defaultdict
 
-NUM_TEAMS = 32
+POPULATION_SIZE = 10000
+GENERATIONS = 100
+FITNESS = int(POPULATION_SIZE * 0.25)
+
+NUM_TEAMS = 10
 ROSTER_SIZE = dict(
     QB=1,
     WR=2,
     RB=2,
     TE=1,
-    #K=1,
+    K=1,
+    DEF=1,
 )
+
+DRAFT_ORDERS_AGE = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))
 
 def read_data():
     draft_data = {}
@@ -69,58 +75,85 @@ def draft(draft_data, draft_orders):
     return draft_results
 
 
-def spawn_next_generation(current_generation, draft_results):
-    # process results
-    for team_results in draft_results:
+def record_draft_results(current_generation, draft_results):
+    # sort results
+    for team in range(len(draft_results)):
+        team_results = draft_results[team]
         team_results.sort(
             key=lambda players: sum([float(player['fpts']) for player in players]),
             reverse=True
         )
+        for draft_result in team_results:
+            draft_orders_key = '-'.join([player['position'] for player in draft_result])
+            DRAFT_ORDERS_AGE[team][draft_orders_key]['age'] += 1
+            DRAFT_ORDERS_AGE[team][draft_orders_key]['total'] += sum([float(player['fpts']) for player in draft_result])
 
     print 'Generation', current_generation, 'Results'
     for team in range(len(draft_results)):
         print 'Team', team, 'draft:'
         for draft_number in range(10):
             draft_result = draft_results[team][draft_number]
+            display_orders = '-'.join([player['position'] for player in draft_result])
             print ' ', sum([float(player['fpts']) for player in draft_result]),
-            print '(' + '-'.join([player['position'] for player in draft_result]) + ')'
+            print '(' + display_orders + ')',
+            print int(DRAFT_ORDERS_AGE[team][display_orders]['age']), DRAFT_ORDERS_AGE[team][display_orders]['total'] / DRAFT_ORDERS_AGE[team][display_orders]['age']
         print
 
+
+def spawn_next_generation(draft_results):
+    # select the top fit team drafts
     fit_orders_by_team = [[] for _ in range(NUM_TEAMS)]
     for team in range(len(draft_results)):
         for draft_number in range(FITNESS):
             fit_orders_by_team[team].append([player['position']
                                              for player in draft_results[team][draft_number]])
 
+    # make hybrid team drafts from fit parents
+    # mutate team drafts
+
+    # pair up top team drafts to make full draft for next generation
     next_gen_draft_orders = []
-    for _ in range(1000): # XXX not sure what to call this magic number... matings?
+    # XXX not sure what to call this magic number... matings?
+    for _ in range(int(POPULATION_SIZE * 0.75)):
         draft_orders = []
         for team_draft_orders in fit_orders_by_team:
             draft_orders.append(random.choice(team_draft_orders))
-        #pprint.pprint(draft_orders)
         next_gen_draft_orders.append(draft_orders)
 
     return next_gen_draft_orders
+
+
+def get_sort_value(d):
+    return d[1]['total'] / d[1]['age']
 
 
 def main():
     draft_data = read_data()
 
     generation_draft_orders = []
-    for _ in range(POPULATION_SIZE):
-        draft_orders = []
-        for team in range(NUM_TEAMS):
-            draft_orders.append(get_random_pp_draft_order())
-        generation_draft_orders.append(draft_orders)
-
     for generation in range(GENERATIONS):
+        for _ in range(POPULATION_SIZE - len(generation_draft_orders)):
+            draft_orders = []
+            for team in range(NUM_TEAMS):
+                draft_orders.append(get_random_pp_draft_order())
+            generation_draft_orders.append(draft_orders)
+
         draft_results = [[] for _ in range(NUM_TEAMS)]
         for draft_orders in generation_draft_orders:
             results = draft(draft_data, draft_orders)
             for team in range(NUM_TEAMS):
                 draft_results[team].append(results[team])
 
-        generation_draft_orders = spawn_next_generation(generation, draft_results)
+        record_draft_results(generation, draft_results)
+        generation_draft_orders = spawn_next_generation(draft_results)
+
+    for team, drafts in DRAFT_ORDERS_AGE.iteritems():
+        print 'Summary for team', team, ':'
+        sorted_orders_by_age = sorted(drafts.items(), key=lambda d: get_sort_value(d), reverse=True)
+        for i in range(10):
+            orders, orders_age = sorted_orders_by_age[i]
+            print ' ', int(orders_age['age']), (orders_age['total'] / orders_age['age']), '(' + orders + ')'
+        print
 
 
 if __name__ == '__main__':
